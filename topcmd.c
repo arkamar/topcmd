b10eaacf6cc00a61c0dbc4ac4e4fa069d5ed62b6
Petr Vaněk <arkamar@atlas.cz>
2020-04-24 10:34:00 +0200

    pipe fork exec

--- topcmd.c
+++ topcmd.c
@@ -15,6 +15,7 @@ main(int argc, char * argv[]) {
 	for (;;) {
 		char buf[BUFSIZ];
 		ssize_t len;
+		int p[2];
 		int ret;
 
 		ret = poll(pfd, LEN(pfd), -1);
@@ -32,6 +33,33 @@ main(int argc, char * argv[]) {
 			} else if (len == 0) {
 				break;
 			}
+
+			if (pipe(p) == -1) {
+				perror("pipe");
+				break;
+			}
+
+			int pid = fork();
+			if (pid == -1) {
+				perror("fork");
+				break;
+			} else if (pid == 0) { /* child */
+				close(p[0]);
+				dup2(p[1], STDOUT_FILENO);
+				dup2(p[1], STDERR_FILENO);
+				if (p[1] != STDOUT_FILENO) {
+					close(p[1]);
+				}
+				if (argc >= 1) {
+					execvp(argv[0], argv);
+					perror("execvp");
+				}
+				_exit(127);
+			} /* end child */
+
+			close(p[0]);
+			close(p[1]);
+
 		}
 
 		if (pfd[0].revents & POLLHUP) {
