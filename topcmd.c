8617a8be32d6b99bfdd06ccae1745a2b411a26a9
Petr Vaněk <arkamar@atlas.cz>
2020-04-24 17:45:22 +0200

    do_header and run_cmd

--- topcmd.c
+++ topcmd.c
@@ -1,8 +1,65 @@
 #include <poll.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
+#include <wchar.h>
 
 #define LEN(x) ( sizeof x / sizeof * x )
+
+static
+void
+do_header() {
+	time_t t = time(NULL);
+	char * ts = ctime(&t);
+	puts(ts);
+}
+
+static
+int
+run_cmd(char * const argv[]) {
+	int pfd[2];
+
+	if (pipe(pfd) == -1) {
+		perror("pipe");
+		return -1;
+	}
+
+	int pid = fork();
+	if (pid == -1) {
+		perror("fork");
+		return -1;
+	} else if (pid == 0) { /* child */
+		close(pfd[0]);
+		dup2(pfd[1], STDOUT_FILENO);
+		dup2(pfd[1], STDERR_FILENO);
+		if (pfd[1] != STDOUT_FILENO) {
+			close(pfd[1]);
+		}
+		if (*argv) {
+			execvp(argv[0], argv);
+			perror("execvp");
+		}
+		_exit(127);
+	} /* end child */
+
+	/* parent continues here */
+
+	close(pfd[1]);
+	FILE * p = fdopen(pfd[0], "r");
+	if (p == NULL) {
+		perror("fdopen");
+		exit(1);
+	}
+
+	for (int i = 0 ; i < 200; i++) {
+		wint_t c = fgetwc(p);
+		//addnwstr((wchar_t *)&c, 1);
+	}
+	fclose(p);
+
+	return 0;
+}
 
 int
 main(int argc, char * argv[]) {
@@ -15,7 +72,6 @@ main(int argc, char * argv[]) {
 	for (;;) {
 		char buf[BUFSIZ];
 		ssize_t len;
-		int p[2];
 		int ret;
 
 		ret = poll(pfd, LEN(pfd), -1);
@@ -34,31 +90,10 @@ main(int argc, char * argv[]) {
 				break;
 			}
 
-			if (pipe(p) == -1) {
-				perror("pipe");
+			do_header();
+			if (run_cmd(argv)) {
 				break;
 			}
-
-			int pid = fork();
-			if (pid == -1) {
-				perror("fork");
-				break;
-			} else if (pid == 0) { /* child */
-				close(p[0]);
-				dup2(p[1], STDOUT_FILENO);
-				dup2(p[1], STDERR_FILENO);
-				if (p[1] != STDOUT_FILENO) {
-					close(p[1]);
-				}
-				if (argc >= 1) {
-					execvp(argv[0], argv);
-					perror("execvp");
-				}
-				_exit(127);
-			} /* end child */
-
-			close(p[0]);
-			close(p[1]);
 
 		}
 
